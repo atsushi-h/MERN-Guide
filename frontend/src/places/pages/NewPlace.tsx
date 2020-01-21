c8225692949c50c3779ddae3e319c2557d15f56f
atsushi-h <atsushi.1213eng@gmail.com>
2020-01-21 09:15:46 +0900

    add page & route

---
+++ frontend/src/places/pages/NewPlace.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewPlace: React.FC = () => (
+  <h2>NewPlace Works!</h2>
+);
+
+export default NewPlace;
---
+++ frontend/src/user/pages/Users.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Users: React.FC = () => (
+  <h2>Users Works!</h2>
+);
+
+export default Users;
