843b109cc8a56840e4185d23c937b017f121804b
atsushi-h <atsushi.1213eng@gmail.com>
2020-01-23 21:23:16 +0900

    Animationg the SideDrawer

--- frontend/src/shared/components/Navigation/MainNavigation.tsx
+++ frontend/src/shared/components/Navigation/MainNavigation.tsx
@@ -9,21 +9,19 @@ import './MainNavigation.css';
 
 const MainNavigation: React.FC = () => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-  const openDrawer = () => setDrawerIsOpen(true);
-  const closeDrawer = () => setDrawerIsOpen(false);
+  const openDrawerHandler = () => setDrawerIsOpen(true);
+  const closeDrawerHandler = () => setDrawerIsOpen(false);
 
   return (
     <>
-      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
-      {drawerIsOpen && (
-        <SideDrawer>
-          <nav className="main-navigation__drawer-nav">
-            <NavLinks /> 
-          </nav>
-        </SideDrawer>
-      )}
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler} >
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks /> 
+        </nav>
+      </SideDrawer>
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
           <span />
           <span />
           <span />
