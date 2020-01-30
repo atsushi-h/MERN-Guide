c2e449fcb9349263deeaadf2e174d59e2b21800f
atsushi-h <atsushi.1213eng@gmail.com>
2020-01-30 15:09:30 +0900

    Add Routing Error

--- backend/app.ts
+++ backend/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 
 import placesRoutes from './routes/places-routes';
+import HttpError from './models/http-error';
 
 const app = express();
 
@@ -9,11 +10,16 @@ app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes); // => /api/places/...
 
+// Routing Error
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
 interface Error {
   code: number,
   message: string,
 }
-
 // Error Handling
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (res.headersSent) {
