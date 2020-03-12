077058b1a14d23a37816c62090400f5a22db7123
atsushi-h <atsushi.1213eng@gmail.com>
2020-03-12 23:33:14 +0900

    Authorization on "Create Places"
    トークンで認証したユーザIDを使用する

--- backend/controllers/places-controller.ts
+++ backend/controllers/places-controller.ts
@@ -75,7 +75,7 @@ export const createPlace = async (req: Request, res: Response, next: NextFunctio
     next(new HttpError('Invalid inputs passed, please check your data.', 422));
   }
 
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates: {
     lat: number,
@@ -87,50 +87,59 @@ export const createPlace = async (req: Request, res: Response, next: NextFunctio
     return next(error);
   }
 
-  const createdPlace = new Place({
-    title,
-    description,
-    image: req.file.path,
-    address,
-    location: coordinates,
-    creator,
-  });
+  // トークンで認証したユーザIDを使用
+  if (req.userData) {
+    const createdPlace = new Place({
+      title,
+      description,
+      image: req.file.path,
+      address,
+      location: coordinates,
+      creator: req.userData.userId,
+    });
 
-  let user;
-  try {
-    user = await User.findById(creator);
-  } catch (err) {
+    let user;
+    try {
+      user = await User.findById(req.userData.userId);
+    } catch (err) {
+      const error = new HttpError(
+        'Creating place failed, please try again',
+        500
+      );
+      return next(error);
+    }
+
+    if (!user) {
+      const error = new HttpError(
+        'Could not find user for provided id',
+        404
+      );
+      return next(error);
+    }
+
+    try {
+      const sess = await mongoose.startSession();
+      sess.startTransaction();
+      await createdPlace.save({ session: sess });
+      user.places.push(createdPlace);
+      await user.save({ session: sess });
+      await sess.commitTransaction();
+    } catch (err) {
+      const error = new HttpError(
+        'Creating place failed, please try again.',
+        500
+      );
+      return next(error);
+    }
+
+    res.status(201).json({ place: createdPlace });
+  } else {
     const error = new HttpError(
       'Creating place failed, please try again',
       500
     );
     return next(error);
   }
-
-  if (!user) {
-    const error = new HttpError(
-      'Could not find user for provided id',
-      404
-    );
-    return next(error);
-  }
-
-  try {
-    const sess = await mongoose.startSession();
-    sess.startTransaction();
-    await createdPlace.save({ session: sess });
-    user.places.push(createdPlace);
-    await user.save({ session: sess });
-    await sess.commitTransaction();
-  } catch (err) {
-    const error = new HttpError(
-      'Creating place failed, please try again.',
-      500
-    );
-    return next(error);
-  }
-
-  res.status(201).json({ place: createdPlace });
 };
 
 export const updatePlace = async (req: Request, res: Response, next: NextFunction) => {
